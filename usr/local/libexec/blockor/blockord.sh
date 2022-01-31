35a422c06b45b3900789d07db06c1026ae27ba2c
Muktadiur Rahman <muktadiur@gmail.com>
2022-01-31 20:49:45 +0600

    ip addr as user issue fixed

--- usr/local/libexec/blockor/blockord.sh
+++ usr/local/libexec/blockor/blockord.sh
@@ -9,7 +9,7 @@ OS=$(uname -s | tr '[A-Z]' '[a-z]')
 
 tail -n 0 -f $auth_file | while read line
 do 
-	echo $line | grep -E "$search_pattern" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' >> $blockor_file
+	echo $line | grep -E "$search_pattern" | grep -oE 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $2}' >> $blockor_file
 
 	for white_ip in $(echo $blockor_whitelist); do
 		if [ $OS = 'openbsd' ]; then
